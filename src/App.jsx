6c018c2b4fc3123f7421aacfe72c94b0f4938f85
Carlos Barçante <barcantecarlosdev@gmail.com>
2023-01-26 16:33:30 -0300

    feat: start to add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './App.css';
 
@@ -7,13 +8,17 @@ import Home from './pages/Home';
 import Lessons from './pages/Lessons';
 import About from './pages/About';
 
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/lessons', element: <Lessons /> },
+  { path: '/about', element: <About /> },
+])
+
 function App() {
   return (
     <div className="App">
       <Nav />
-      <Home />
-      <Lessons />
-      <About />
+      <RouterProvider router={router} />
     </div>
   )
 }
